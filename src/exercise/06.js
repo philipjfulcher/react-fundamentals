8a3c4dbc4042dde7ad43f9caf9e6a0a265fe31cc
Philip Fulcher <philip@nrwl.io>
2020-10-05 22:19:23 -0600

    completed exercise 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,37 +1,42 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React from 'react'
+import React from 'react';
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0]
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const [userName, setUserName] = React.useState('');
 
-  // 🐨 add the onSubmit handler to the <form> below
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmitUsername(userName);
+  }
 
-  // 🐨 make sure to associate the label to the input by specifying an `id` on
-  // the input and a matching value as an `htmlFor` prop on the label.
+  function handleChange(event) {
+    const value = event.target.value;
+    setUserName(value.toLowerCase());
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="userName">Username:</label>
+        <input
+          onChange={handleChange}
+          value={userName}
+          id="userName"
+          type="text"
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit">
+        Submit
+      </button>
     </form>
-  )
+  );
 }
 
 function App() {
-  const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  const onSubmitUsername = username => alert(`You entered: ${username}`);
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />;
 }
 
-export default App
+export default App;
